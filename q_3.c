46f0f1cf1fbaf9ad6e60b9aa5634cd73c897fed2
yadu2000 <56623467+yadu2000@users.noreply.github.com>
2019-10-16 12:10:46 +0530

    Create q_3.c

---
+++ q_3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+
+
+ int i,j;
+
+         for(i=0;i<=4;i++)
+         {
+         for(j=0;j<=i;j++)
+         {
+         if((i+j)%2==0)
+         {
+    printf("1");
+         }
+      else
+         {
+    printf("0");
+         }
+         }
+    printf("\n");
+
+
+  }
+}
